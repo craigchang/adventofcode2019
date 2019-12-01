18c337ae3575fe0d1e599c8ee8773b1219231b84
Craig Chang <craigchang@gmail.com>
2019-12-01 12:02:06 -0800

    day1

---
+++ day1/day1.py
@@ -0,0 +1,33 @@
+import math
+
+def readFile():
+    file = open("day1/input.txt", "r")
+    massList = [int(x) for x in file]
+    file.close()
+    return massList
+
+
+def calculateFuel(mass):
+    return math.floor(mass / 3) - 2
+
+def part1(massList):
+    return sum([calculateFuel(mass) for mass in massList])
+
+def part2 (massList):
+    sumFuel = 0
+    for mass in massList:
+        sumFeulForMass = 0
+        while True:
+            remainingFuel = calculateFuel(mass)
+            if (remainingFuel <= 0):
+                break
+            sumFeulForMass += remainingFuel
+            mass = remainingFuel
+        sumFuel += sumFeulForMass
+
+    return sumFuel
+
+massList = readFile()
+print(part1(massList))
+print(part2(massList))
+
